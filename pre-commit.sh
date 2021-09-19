f1da5e7fea59b8437fcc5bae5372c7d3426bef7d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-09-19 22:35:33 +0200

    Hopefully fix

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -ex
-shopt -s extglob
 
 ln -sf ../../pre-commit.sh .git/hooks/pre-commit
 
@@ -16,10 +15,11 @@ cargo test
 cargo clean # needed to find out the object later
 RUSTFLAGS="-Z instrument-coverage" cargo test
 cargo profdata -- merge -sparse default.profraw -o default.profdata
-cargo cov -- show --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile=default.profdata --object target/debug/deps/ical_rrule-b2a70cd2ca5a8a21 --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
-cargo cov -- show --format=html --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile=default.profdata --object target/debug/deps/ical_rrule-b2a70cd2ca5a8a21 --output-dir=target/debug/coverage --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
+rm target/debug/deps/*.d
+cargo cov -- show --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile=default.profdata --object target/debug/deps/ical_rrule-* --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
+cargo cov -- show --format=html --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile=default.profdata --object target/debug/deps/ical_rrule-* --output-dir=target/debug/coverage --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
 firefox target/debug/coverage/index.html
-cargo cov -- report --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile=default.profdata --object target/debug/deps/ical_rrule-b2a70cd2ca5a8a21
+cargo cov -- report --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile=default.profdata --object target/debug/deps/ical_rrule-*
 
 #~/.cargo/bin/grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/
 
