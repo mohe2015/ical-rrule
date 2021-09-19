2a57d9b14a4c29cf6c4323ca784d165e1924a9c1
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-09-19 21:13:16 +0200

    Create code coverage

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,3 +7,10 @@ cargo fmt -- --check
 cargo build
 cargo clippy
 cargo test
+# https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/instrument-coverage.html
+RUSTFLAGS="-Z instrument-coverage" cargo test
+rustup component add llvm-tools-preview
+cargo install cargo-binutils rustfilt
+cargo profdata -- merge -sparse default.profraw -o default.profdata
+cargo cov -- report --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile=default.profdata --object target/debug/deps/ical_rrule-b2a70cd2ca5a8a21
+cargo cov -- show --use-color --ignore-filename-regex='/.cargo/registry' --instr-profile=default.profdata --object target/debug/deps/ical_rrule-b2a70cd2ca5a8a21 --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
