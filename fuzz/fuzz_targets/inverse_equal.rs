acd6a8bb34639a9ee1b7a7a365926d7fa085eeb7
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-09-24 23:40:09 +0200

    Remove println

--- fuzz/fuzz_targets/inverse_equal.rs
+++ fuzz/fuzz_targets/inverse_equal.rs
@@ -5,7 +5,7 @@ use ical_rrule::parser::rrule;
 
 fuzz_target!(|data: RecurRule| {
     let val = data.to_string();
-    println!("{}", val);
+    //println!("{}", val);
     let parsed = rrule(&val);
     /*match &parsed {
         Err(nom::Err::Error(nom::error::Error { input: _, code: nom::error::ErrorKind::Verify })) => {
