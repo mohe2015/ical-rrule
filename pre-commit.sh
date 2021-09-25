96c8d55e6b43f9a7d5204c7f390997cacc7453ae
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2021-09-25 22:56:14 +0200

    Pre-commit coverage fixed

--- pre-commit.sh
+++ pre-commit.sh
@@ -9,17 +9,13 @@ cargo install cargo-binutils rustfilt cargo-audit grcov
 export RUSTFLAGS="-D warnings -Z instrument-coverage"
 cargo fmt -- --check
 # https://doc.rust-lang.org/nightly/unstable-book/compiler-flags/instrument-coverage.html
-#cargo clean # needed to find out the object later
-cargo test
+binary=$(cargo test --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]")
+$binary
 cargo clippy
 cargo profdata -- merge -sparse default.profraw -o default.profdata
-cargo cov -- show --use-color --ignore-filename-regex='(/.cargo/registry)|(/rustc/)' --instr-profile=default.profdata --object $(cargo test --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]") --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
-cargo cov -- show --format=html --use-color --ignore-filename-regex='(/.cargo/registry)|(/rustc/)' --instr-profile=default.profdata --object $(cargo test --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]") --output-dir=target/debug/coverage --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
+cargo cov -- show --use-color --ignore-filename-regex='(/.cargo/registry)|(/rustc/)' --instr-profile=default.profdata --object $binary --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
+cargo cov -- show --format=html --use-color --ignore-filename-regex='(/.cargo/registry)|(/rustc/)' --instr-profile=default.profdata --object $binary --output-dir=target/debug/coverage --show-instantiations --show-line-counts-or-regions --Xdemangler=rustfilt
 firefox target/debug/coverage/index.html
-cargo cov -- report --use-color --ignore-filename-regex='(/.cargo/registry)|(/rustc/)' --instr-profile=default.profdata --object $(cargo test --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]")
-
-# bad coverage for unused instantiations
-#~/.cargo/bin/grcov . -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/grcov/
-#firefox target/debug/grcov/index.html
+cargo cov -- report --use-color --ignore-filename-regex='(/.cargo/registry)|(/rustc/)' --instr-profile=default.profdata --object $binary
 
 cargo audit
